ec983e06163b46a56782364f781115f22a931fb7
Xiaohui-Jiang <xhjiang64@gmail.com>
2024-05-01 20:38:51 -0400

    testGUI

--- example_usage.py
+++ example_usage.py
@@ -1,11 +1,11 @@
 """Example Usage."""
 
-from property_calculation import (
+from src.property_calculation import (
     compute_similarity,
     extract_image_features,
     plot_heatmap,
 )
-from segmentationGUI import main as segmentation_main
+from src.SegmentationGUI import main as segmentation_main
 
 # Functionality 1: Segmentation GUI
 segmentation_main()
